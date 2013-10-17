b5ecbef76679de1566dba2ea89843ad42b049911
Stu Bailey <sbailey@infoblox.com>
2013-10-17 14:14:15 -0700

    Finall code structure

---
+++ Makefile
@@ -0,0 +1,42 @@
+.PHONY: rel compile lib get-deps update-deps test clean deep-clean
+
+rel: compile
+#	@./rebar generate -f
+#	@./scripts/post_generate_hook
+
+offline:
+	@./rebar compile
+#	erl -pa ebin
+#	@./rebar generate -f
+#	@./scripts/post_generate_hook
+
+lib:
+	cd lib; make all
+
+compile: lib get-deps update-deps
+	@./rebar compile
+
+get-deps:
+	@./rebar get-deps
+
+update-deps:
+	@./rebar update-deps
+
+test: offline
+	@./rebar skip_deps=true apps="loom" eunit
+
+test_us3: compile
+	@./rebar skip_deps=true apps="loom" eunit
+
+clean:
+	@./rebar clean
+
+deep-clean: clean
+	@./rebar delete-deps
+
+setup_dialyzer:
+	dialyzer --build_plt --apps erts kernel stdlib mnesia compiler syntax_tools runtime_tools crypto tools inets ssl webtool public_key observer
+	dialyzer --add_to_plt deps/*/ebin
+
+dialyzer: compile
+	dialyzer apps/*/ebin
---
+++ www/static/chart.js
@@ -0,0 +1,42 @@
+function initChart (){
+   var options = {
+	   width: 700,
+	   height: 400,
+	   legend: "none",
+       title: '',
+      // vAxis: {title: 'Year',  titleTextStyle: {color: 'red'}},
+	   animation: {
+		   duration: 1000,
+		   easing: 'out'
+	   }
+   };
+   
+   var data = google.visualization.arrayToDataTable([
+     ['Hour', 'Index'],
+     ['2a.m',  1000],
+     ['3a.m',  1170],
+     ['4a.m',  660],
+     ['5a.m',  1030],
+	 ['6a.m',  700],
+	 ['7a.m',  1110],
+	 ['7a.m',  1110],
+	 ['7a.m',  1110],
+   ]);
+   
+   var chart = new google.visualization.BarChart(document.getElementById('visualization'));
+   
+   // data.addColumn('string', 'x');
+   // data.addColumn('number', 'y');
+   // data.addRow(['100', 123]);
+   // data.addRow(['700', 17]);
+   var button = document.getElementById('b1');
+   function drawChart() {
+	   chart.draw(data, options);
+   };
+   // button.onclick = function() {
+   //   var newValue = 1000 - data.getValue(0, 1);
+   //   data.setValue(0, 1, newValue);
+   //   drawChart();
+   // };
+   drawChart();
+}
