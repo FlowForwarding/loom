52fb2f739277f55e446e243d1079e3a712efaa68
Stu Bailey <sbailey@infoblox.com>
2013-10-18 09:40:58 -0700

    Added Websocket support

---
+++ src/clientsock.erl
@@ -0,0 +1,25 @@
+-module(clientsock).
+
+-compile([export_all]).
+
+-include("../include/tapestry.hrl").
+
+handle_message({text, <<"PING">>}) ->
+    Pid = self(),
+    register(client,self()),
+    error_logger:info_msg("Received PING:~n"),
+    error_logger:info_msg("Client Registered as {client,~p}~n",[Pid]),
+    {reply, {text, <<"PONG">>}};
+handle_message(A)->
+    error_logger:info_msg("Received:~n~p~n",[A]),
+    error_logger:info_msg("My Pid = ~p~n",[self()]),
+    noreply.
+
+send(ID,Message) when is_atom(ID) ->
+    Pid = whereis(ID),
+    send(Pid,Message);
+send(Pid,Message) when is_pid(Pid) ->
+    Term = term_to_binary(Message),
+    Payload = <<?BERT_VERSION,Term/binary>>,
+    yaws_api:websocket_send(Pid, {binary, Term}).
+
