f161058074ee79efbaba0af578a257cf99ba536e
Iryna Dastanka <Iryna_Dastanka@epam.com>
2013-10-18 17:26:29 +0300

    Change chart size for small screens

--- www/static/chart.js
+++ www/static/chart.js
@@ -1,7 +1,9 @@
 function initChart (){
-	var chartWidth = 3*$( window ).width()/4;
+	var chartWidth = 3 *$( window ).width()/4;
 	if (chartWidth > 700)
 		chartWidth = 700;
+    if ($( window ).width() < 400) 	
+	  chartWidth = 3*$( window ).width()/5; 
    var options = {
 	   width: chartWidth,
 	   height: $( window ).height()/2,
