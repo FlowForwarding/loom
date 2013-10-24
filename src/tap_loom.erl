37460b942ca2644094a16cc39bdeaf24084d84b7
Stu Bailey <sbailey@infoblox.com>
2013-10-24 10:33:16 -0700

    integration

--- src/tap_loom.erl
+++ src/tap_loom.erl
@@ -6,6 +6,9 @@
 
 get_ofdp_recv_list()->
     LoomSupTree = loom:get_sup_tree(),
+    get_ofdp_recv_list(LoomSupTree).
+
+get_ofdp_recv_list(LoomSupTree)->
     TapLoom = lists:keyfind(dns_tap,1,LoomSupTree),
     TapChildren = case TapLoom of
 		      false ->
@@ -18,12 +21,48 @@ get_ofdp_recv_list()->
 		[] -> false;
 		_ -> {OFDP,Rest} = lists:partition(fun(X)->
 							   [Name | Rest] = tuple_to_list(X),
-							   Name == loom_odfp_recv end,TapChildren),
+							   Name == loom_ofdp_recv_sup end,TapChildren),
 		     OFDP
 	    end,
-    case OFDPL of
-	[] -> false;
-	_ -> OFDPL
-    end.
+    Workers = case OFDPL of
+		  [] -> false;
+		  [{_,_,W}] -> W;
+		  _ -> false
+	      end,
+    lists:foldl(fun(X,AccIn)->case X of
+				  {_,Pid,worker,[loom_ofdp_recv]} ->
+				      [Pid|AccIn];
+				  _ -> AccIn
+			      end
+		end,[],Workers).
+	
 	     
 				     
+get_ofdp_recv_list_test()->
+    LoomSupTree = [{dns_tap,{dns_tap,"<0.111.0>",supervisor,
+                   [loom_controller_sup]},
+          [{loom_ofdp_recv_sup,{loom_ofdp_recv_sup,"<0.115.0>",
+                                                   supervisor,
+                                                   [loom_ofdp_recv_sup]},
+                               [{undefined,"<0.121.0>",worker,[loom_ofdp_recv]}]},
+           {loom_ofdp_sup,{loom_ofdp_sup,"<0.114.0>",supervisor,
+                                         [loom_ofdp_sup]},
+                          [{undefined,"<0.119.0>",worker,[loom_ofdp]}]},
+           {loom_c_listen_sup,{loom_c_listen_sup,"<0.113.0>",supervisor,
+                                                 [loom_c_listen_sup]},
+                              [{undefined,"<0.116.0>",worker,[loom_c_listen]}]},
+           {loom_controller,"<0.112.0>",worker,[loom_controller]}]},
+ {default,{default,"<0.105.0>",supervisor,
+                   [loom_controller_sup]},
+          [{loom_ofdp_recv_sup,{loom_ofdp_recv_sup,"<0.109.0>",
+                                                   supervisor,
+                                                   [loom_ofdp_recv_sup]},
+                               [{undefined,"<0.120.0>",worker,[loom_ofdp_recv]}]},
+           {loom_ofdp_sup,{loom_ofdp_sup,"<0.108.0>",supervisor,
+                                         [loom_ofdp_sup]},
+                          [{undefined,"<0.118.0>",worker,[loom_ofdp]}]},
+           {loom_c_listen_sup,{loom_c_listen_sup,"<0.107.0>",supervisor,
+                                                 [loom_c_listen_sup]},
+                              [{undefined,"<0.110.0>",worker,[loom_c_listen]}]},
+           {loom_controller,"<0.106.0>",worker,[loom_controller]}]}],
+   get_ofdp_recv_list(LoomSupTree).
