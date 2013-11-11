2b28816eeeecb634a30a00221459e3050f331a20
Iryna Dastanka <Iryna_Dastanka@epam.com>
2013-11-11 17:25:02 +0300

    Very basics to organize websoket connection
    Setup websocket connection
    Start recieving live data and show NCI, NEP and QPS indexes

--- src/ui/mobile/ios/Tapestry/Tapestry/NCIGraphController.m
+++ src/ui/mobile/ios/Tapestry/Tapestry/NCIGraphController.m
@@ -7,10 +7,18 @@
 //
 
 #import "NCIGraphController.h"
+#import "SRWebSocket.h"
 
-@interface NCIGraphController ()
-
+@interface NCIGraphController() <SRWebSocketDelegate>{
+    SRWebSocket *socket;
+    UILabel *nciValue;
+    UILabel *nepValue;
+    UILabel *qpsValue;
+}
 @end
+
+static NSString* websocketUrl = @"secret";
+static NSString* websocketStartRequest = @"START_DATA";
 
 @implementation NCIGraphController
 
@@ -26,13 +34,78 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    int topIndent = 100;
+    int indexLabelHeight = 30;
     self.title = NSLocalizedString(@"Tapestry: A Network Complexity Analyzer", nil);
+    nciValue = [[UILabel alloc] initWithFrame:CGRectMake(0, topIndent, self.view.bounds.size.width/2, indexLabelHeight)];
+    [self.view addSubview:nciValue];
+    nepValue = [[UILabel alloc] initWithFrame:CGRectMake(self.view.bounds.size.width/2,
+                                                         topIndent, self.view.bounds.size.width/2, indexLabelHeight)];
+    [self.view addSubview:nepValue];
+    
+    
+    qpsValue = [[UILabel alloc] initWithFrame:CGRectMake(self.view.bounds.size.width/2,
+                                                         topIndent + indexLabelHeight, self.view.bounds.size.width/2, indexLabelHeight)];
+    [self.view addSubview:qpsValue];
+    
+    
+    [self reconnect];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+- (void)reconnect;
+{
+    socket.delegate = nil;
+    [socket close];
+    socket = [[SRWebSocket alloc] initWithURLRequest: [NSURLRequest requestWithURL:[NSURL URLWithString:websocketUrl]]];
+    socket.delegate = self;
+    [socket open];
+    
+}
+
+#pragma mark - SRWebSocketDelegate
+
+- (void)webSocketDidOpen:(SRWebSocket *)webSocket;
+{
+    NSLog(@"Websocket Connected");
+    [webSocket send:websocketStartRequest];
+}
+
+- (void)webSocket:(SRWebSocket *)webSocket didFailWithError:(NSError *)error;
+{
+    NSLog(@"Websocket Failed With Error %@", error);
+    webSocket = nil;
+}
+
+- (void)webSocket:(SRWebSocket *)webSocket didReceiveMessage:(id)message;
+{
+    NSDictionary *dataPoint = [NSJSONSerialization
+                               JSONObjectWithData:[message dataUsingEncoding:NSUTF8StringEncoding]
+                               options:NSJSONReadingMutableContainers error:NULL];
+    
+   // NSLog(@"dataPoint %@", dataPoint);
+    NSString *nci = dataPoint[@"NCI"];
+    NSString *nep = dataPoint[@"NEP"];
+    NSString *qps = dataPoint[@"QPS"];
+    if (nci){
+        nciValue.text = [NSString stringWithFormat: @"NCI %@  : %@", nci, dataPoint[@"Time"]];
+    } else if (nep) {
+        nepValue.text = [NSString stringWithFormat: @"NEP %@  : %@", nep, dataPoint[@"Time"]];
+    } else if (qps) {
+        qpsValue.text = [NSString stringWithFormat: @"QPS %@  : %@", qps, dataPoint[@"Time"]];
+    }
+}
+
+- (void)webSocket:(SRWebSocket *)webSocket didCloseWithCode:(NSInteger)code reason:(NSString *)reason wasClean:(BOOL)wasClean;
+{
+    NSLog(@"WebSocket closed");
+    socket = nil;
 }
 
 @end
