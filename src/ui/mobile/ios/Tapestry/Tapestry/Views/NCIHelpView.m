49789302ac9a490b7bdcd2cc1aea33cb9e3b2d20
Iryna Dastanka <Iryna_Dastanka@epam.com>
2013-11-27 13:28:02 +0300

    Make up Help Menu

--- src/ui/mobile/ios/Tapestry/Tapestry/Views/NCIHelpView.m
+++ src/ui/mobile/ios/Tapestry/Tapestry/Views/NCIHelpView.m
@@ -9,14 +9,16 @@
 #import "NCIHelpView.h"
 
 @interface NCIHelpView(){
-    UIView *topView;
-    UIView *shadowView;
     int topViewHeigth;
     int shadowHeigth;
-    CAGradientLayer * gradient;
+    UIButton *aboutLink;
+    UIButton *nciLink;
+    UIButton *ffLink;
 }
 
 @end
+
+int btnHeight = 40;
 
 @implementation NCIHelpView
 
@@ -29,56 +31,41 @@
         self.backgroundColor = [UIColor colorWithWhite:0.1 alpha:0.2];
         self.hidden = YES;
         
-        topView = [[UIView alloc] initWithFrame:CGRectMake(0, -topViewHeigth, self.frame.size.width, topViewHeigth)];
-        topView.backgroundColor = [UIColor whiteColor];
-        [self addSubview:topView];
+        aboutLink = [[UIButton alloc] initWithFrame:CGRectZero];
+        [aboutLink setTitle: NSLocalizedString(@"About NCI", nil) forState:UIControlStateNormal];
+        [self makeupButton: aboutLink];
+        [aboutLink addTarget:self action:@selector(gotoAbout) forControlEvents:UIControlEventTouchUpInside];
+        [self addSubview:aboutLink];
         
-        shadowView = [[UIView alloc] initWithFrame:CGRectMake(0, topViewHeigth - shadowHeigth, topView.frame.size.width, shadowHeigth)];
-        shadowView.backgroundColor = [UIColor lightGrayColor];
-        [topView addSubview:shadowView];
+        nciLink = [[UIButton alloc] initWithFrame:CGRectZero];
+        [nciLink setTitle: NSLocalizedString(@"NCI – Technical paper", nil) forState:UIControlStateNormal];
+        [self makeupButton: nciLink];
+        [nciLink addTarget:self action:@selector(gotoNCI) forControlEvents:UIControlEventTouchUpInside];
+        [self addSubview:nciLink];
         
-        int leftIndent =  20;
-        int labelWidth = 300;
+        ffLink = [[UIButton alloc] initWithFrame:CGRectZero];
+        [ffLink setTitle: NSLocalizedString(@"About FlowForwarding.Org", nil) forState:UIControlStateNormal];
+        [self makeupButton: ffLink];
+        [ffLink addTarget:self action:@selector(gotoFlowForwarding) forControlEvents:UIControlEventTouchUpInside];
+        [self addSubview:ffLink];
         
-        UILabel *aboutLink = [[UILabel alloc] initWithFrame:CGRectMake(leftIndent, 20, labelWidth,  20)];
-        aboutLink.text = NSLocalizedString(@"About NCI", nil);
-        aboutLink.textColor = [UIColor blueColor];
-        [topView addSubview:aboutLink];
-        aboutLink.userInteractionEnabled = YES;
-        UITapGestureRecognizer *tapAbout = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(gotoAbout)];
-        tapAbout.numberOfTapsRequired = 1;
-        [aboutLink addGestureRecognizer:tapAbout];
-        
-        UILabel *nciLink = [[UILabel alloc] initWithFrame:CGRectMake(leftIndent, 50, labelWidth, 20)];
-        nciLink.text = NSLocalizedString(@"NCI – Technical paper", nil);
-        [topView addSubview:nciLink];
-        nciLink.userInteractionEnabled = YES;
-        nciLink.textColor = [UIColor blueColor];
-        UITapGestureRecognizer *tapNCI = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(gotoNCI)];
-        tapNCI.numberOfTapsRequired = 1;
-        [nciLink addGestureRecognizer:tapNCI];
-        
-        UILabel *ffLink = [[UILabel alloc] initWithFrame:CGRectMake(leftIndent, 80, labelWidth, 20)];
-        ffLink.text = NSLocalizedString(@"About FlowForwarding.Org", nil);
-        [topView addSubview:ffLink];
-        ffLink.userInteractionEnabled = YES;
-        ffLink.textColor = [UIColor blueColor];
-        UITapGestureRecognizer *tapFF = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(gotoFlowForwarding)];
-        tapFF.numberOfTapsRequired = 1;
-        [ffLink addGestureRecognizer:tapFF];
-
-        UITapGestureRecognizer *hideTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(hideHelp)];
-        hideTap.numberOfTapsRequired = 1;
-        self.userInteractionEnabled = YES;
-        [self addGestureRecognizer:hideTap];
+        self.backgroundColor = [UIColor clearColor];
     }
     return self;
 }
 
+- (void)makeupButton:(UIButton *)btn{
+    [btn setTitleColor: [UIColor whiteColor] forState:UIControlStateNormal];
+    btn.backgroundColor = [UIColor colorWithWhite:0.3 alpha:1];
+    [btn setImage: [UIImage imageNamed:@"external_link"] forState:UIControlStateNormal];
+    btn.imageEdgeInsets = UIEdgeInsetsMake(0, 0, 0, -2*[btn.titleLabel.text sizeWithFont:btn.titleLabel.font].width - 60);
+    
+}
+
 - (void)layoutSubviews{
-    topView.frame = CGRectMake(0, topView.frame.origin.y, self.frame.size.width, topViewHeigth);
-    shadowView.frame = CGRectMake(0, topViewHeigth - shadowHeigth, topView.frame.size.width, shadowHeigth);
-    [gradient setFrame:[shadowView bounds]];
+    aboutLink.frame = CGRectMake(0, 1, self.frame.size.width, btnHeight);
+    nciLink.frame = CGRectMake(0, btnHeight + 2, self.frame.size.width, btnHeight);
+    ffLink.frame = CGRectMake(0, 2*btnHeight + 3, self.frame.size.width, btnHeight);
 }
 
 - (void)gotoAbout{
@@ -95,7 +82,7 @@
 
 - (void)hideHelp{
     [UIView animateWithDuration:0.3 animations:^{
-        topView.frame = CGRectMake(0, -topViewHeigth, self.frame.size.width, 0);
+        self.frame = CGRectMake(0, -(btnHeight*3+3), self.frame.size.width, 0);
     } completion:^(BOOL finished) {
         self.hidden = YES;
     }];
@@ -108,7 +95,7 @@
     }
     [UIView animateWithDuration:0.3 animations:^{
         self.hidden = NO;
-        topView.frame = CGRectMake(0, 0, self.frame.size.width, topViewHeigth);
+        self.frame = CGRectMake(0, 0, self.frame.size.width, btnHeight*3+3);
     }];
 }
 
