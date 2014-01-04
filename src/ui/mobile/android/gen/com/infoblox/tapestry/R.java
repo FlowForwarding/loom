67f9888afbd7761db6b1afd955bbea437e52a961
Iryna Dastanka <Iryna_Dastanka@epam.com>
2014-01-04 22:01:39 +0300

    empty menu

--- src/ui/mobile/android/gen/com/infoblox/tapestry/R.java
+++ src/ui/mobile/android/gen/com/infoblox/tapestry/R.java
@@ -22,9 +22,13 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int infoicon=0x7f020001;
+        public static final int link=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int about_flowforwarding=0x7f080002;
+        public static final int about_nci=0x7f080000;
+        public static final int nci_technical_paper=0x7f080001;
     }
     public static final class layout {
         public static final int activity_nci=0x7f030000;
@@ -33,20 +37,18 @@ public final class R {
         public static final int nci=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
+        public static final int about_flowforwarding=0x7f050003;
+        public static final int about_nci=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int hello_world=0x7f050004;
+        public static final int nci_technical_paper=0x7f050002;
     }
     public static final class style {
+        public static final int ActionBar=0x7f060001;
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
     
-
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
 
         Base application theme for API 11+. This theme completely replaces
         AppBaseTheme from res/values/styles.xml on API 11+ devices.
@@ -61,8 +63,7 @@ public final class R {
          */
         public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f060002;
     }
 }
