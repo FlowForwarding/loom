aca2a6b00bc607eb51cb5a40b8299ac807e52d4b
Stu Bailey <sbailey@infoblox.com>
2014-02-14 11:26:52 -0800

    Porting tap_loom.erl to new Loom

--- src/tap_loom.erl
+++ src/tap_loom.erl
@@ -20,7 +20,15 @@
 
 -module(tap_loom).
 
+-include_lib("../lib/loom/deps/ofs_handler/include/ofs_handler.hrl").
+-include_lib("../lib/loom/deps/of_protocol/include/of_protocol.hrl").
+-include_lib("../lib/loom/deps/of_protocol/include/ofp_v4.hrl").
+
 -compile([export_all]).
+
+-define(L_PRIORITY, 100).
+-define(H_PRIORITY, 101).
+-define(DNS_IP, {10,48,2,5}).
 
 start()->
     [code:add_pathz(Path) || Path <- filelib:wildcard("./lib/loom/ebin")],
@@ -64,7 +72,7 @@ process_config([Config|Rest]) ->
 	_ -> process_config(Rest)
     end.
 
-process_config([],IP)->
+process_config([],_IP)->
     ok;
 process_config([Config|Rest],IP) ->
     case Config of
@@ -74,7 +82,7 @@ process_config([Config|Rest],IP) ->
     end.
 
 
-process_ofdps([],TargetIP)->
+process_ofdps([],_TargetIP)->
         ok;
 process_ofdps([OFDP|Rest],TargetIP) ->
     case OFDP of
@@ -88,7 +96,7 @@ process_ofdps([OFDP|Rest],TargetIP) ->
 				  {OFDPIP, DatapathId, Version, _, _} = X,
 				  case OFDPIP == IPAddr of
 				      true ->
-					  dns_tap([DataPathId],Port1,Port2,IPs);
+					  dns_tap([DatapathId],Version,Port1,Port2,IPs);
 				      false -> ok
 				  end
 			  end, OFSwitchList),
@@ -109,15 +117,15 @@ process_ofdps([OFDP|Rest]) ->
 	    {dns_port,Port1} = DNSPort,
 	    {client_port,Port2} = ClientPort,
 	    {dns_ips,IPs} = DNSIps,
-	    OFDPList = loom_ofdp:get_all(default),
+	    OFSwitchList = simple_ne_logic:switches(),
 	    lists:foreach(fun(X)->
-				  {OFDPIP,_} = loom_ofdp:get_address(X),
+				  {OFDPIP, DatapathId, Version, _, _} = X,
 				  case OFDPIP == IPAddr of
 				      true ->
-					  dns_tap([X],Port1,Port2,IPs);
+					  dns_tap([DatapathId],Version,Port1,Port2,IPs);
 				      false -> ok
 				  end
-			  end, OFDPList),
+			  end, OFSwitchList),
 	    process_ofdps(Rest);
 	_ -> process_ofdps(Rest)
     end.
@@ -125,14 +133,37 @@ process_ofdps([OFDP|Rest]) ->
     
 
 
-dns_tap([],_Port1,_Port2,_IPTupleList)->
+dns_tap([],_Version,_Port1,_Port2,_IPTupleList)->
     ok;
-dns_tap(OFDPL,Port1,Port2,IPTupleList)->
+dns_tap(OFDPL,Version,Port1,Port2,IPTupleList)->
     [DatapathId|Rest] = OFDPL,
     IPList = [ list_to_binary(tuple_to_list(IPTuple)) || IPTuple <- IPTupleList ],
-    error_logger:info_msg("dns_tap: ~p, ~p, ~p, ~p~n",[OFDP,Port1,Port2,IPTupleList]),
+    error_logger:info_msg("dns_tap: ~p, ~p, ~p, ~p~n",[DatapathId,Port1,Port2,IPTupleList]),
     ofs_handler:send(DatapathId, remove_all_flows_mod(Version)),
     lists:foreach(fun(X)->ofs_handler:send(DatapathId, tap_dns_response(Version, Port1,Port2,controller,X)) end,IPList),
     ofs_handler:send(DatapathId, forward_mod(Version, Port1, [Port2])),
-    ofs_handler:send(DatapathId, forward_mod(Version, Port2, [Port1]))
-    dns_tap(Rest,Port1,Port2,IPTupleList).
+    ofs_handler:send(DatapathId, forward_mod(Version, Port2, [Port1])),
+    dns_tap(Rest,Version,Port1,Port2,IPTupleList).
+
+
+%tap packets to controller for udp traffic from DNS server IP address
+tap_dns_response(Version, Port1, Port2, Port3, IPv4Src) ->
+%     Matches = [{in_port, <<Port1:32>>}, {eth_type, 2048}, {ip_proto, <<17:8>>}],
+    Matches = [{in_port, <<Port1:32>>}, {eth_type, 2048}, {ip_proto, <<17:8>>}, {ipv4_src, IPv4Src} ],
+    Instructions = [{apply_actions, [{output, Port2, no_buffer}, {output, Port3, no_buffer}] }],
+    Opts = [{table_id,0}, {priority, ?H_PRIORITY}, {idle_timeout, 0}, {idle_timeout, 0},
+            {cookie, <<0,0,0,0,0,0,0,10>>}, {cookie_mask, <<0,0,0,0,0,0,0,0>>}],
+    of_msg_lib:flow_add(Version, Matches, Instructions, Opts).
+
+%forward packet on InPort to OutPorts using apply_actions
+forward_mod(Version,InPort,OutPorts)->
+    Matches = [{in_port, <<InPort:32>>}],
+    Instructions = [{apply_actions, [{output, OutPort, no_buffer} || OutPort <- OutPorts] } ],
+    io:format("Instructions=~p~n", [Instructions]),
+    Opts = [{table_id,0}, {priority,?L_PRIORITY}, {idle_timeout, 0}, {idle_timeout, 0},
+            {cookie, <<0,0,0,0,0,0,0,10>>}, {cookie_mask, <<0,0,0,0,0,0,0,0>>}],
+    of_msg_lib:flow_add(Version, Matches, Instructions, Opts).    
+
+% delete all flows in table 0
+remove_all_flows_mod(Version) ->
+    of_msg_lib:flow_delete(Version, [], [{table_id, 0}]).
