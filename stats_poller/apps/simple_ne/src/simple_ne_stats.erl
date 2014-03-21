35f0ae3af4ae20c21087d6828c7070bc3ae7ea26
marcsugiyama <sugiyama@acm.org>
2014-03-21 11:25:49 -0700

    Push simple_ne down one layer and add tree for stats_poller.

---
+++ stats_poller/apps/simple_ne/src/simple_ne_stats.erl
@@ -0,0 +1,158 @@
+%%------------------------------------------------------------------------------
+%% Copyright 2014 FlowForwarding.org
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%-----------------------------------------------------------------------------
+
+%% @author Erlang Solutions Ltd. <openflow@erlang-solutions.com>
+%% @copyright 2014 FlowForwarding.org
+
+%%% @doc
+%%% Simple network exeuctive open flow stats poller.
+%%% @end
+
+-module(simple_ne_stats).
+
+-behaviour(gen_server).
+-define(SERVER, ?MODULE).
+-define(STATE, simple_ne_stats_state).
+
+-include_lib("loom/include/loom_logger.hrl").
+
+-define(DEFAULT_STATS_INTERVAL, 10).
+-define(DEFAULT_STATS, [flow, table, aggregate, port, queue, group, meter]).
+
+-record(?STATE, {
+    stats,
+    stats_interval, 
+    interval_timer,
+    of_version,
+    datapath_id
+}).
+
+%% ------------------------------------------------------------------
+%% API Function Exports
+%% ------------------------------------------------------------------
+
+-export([start_link/2, stop/2]).
+
+%% ------------------------------------------------------------------
+%% gen_server Function Exports
+%% ------------------------------------------------------------------
+
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
+
+%% ------------------------------------------------------------------
+%% API Function Definitions
+%% ------------------------------------------------------------------
+
+start_link(Version, DatapathId) ->
+    gen_server:start_link(?MODULE, [Version, DatapathId], []).
+
+stop(Pid, Reason) ->
+    gen_server:cast(Pid, {stop, Reason}),
+    ok.
+
+%% ------------------------------------------------------------------
+%% gen_server Function Definitions
+%% ------------------------------------------------------------------
+
+init([Version, DatapathId]) ->
+    Stats = application:get_env(simple_ne, stats, ?DEFAULT_STATS),
+    StatsInterval = application:get_env(simple_ne, stats_interval_sec, ?DEFAULT_STATS_INTERVAL),
+    State = #?STATE{stats = Stats,
+                    stats_interval = StatsInterval,
+                    of_version = Version,
+                    datapath_id = DatapathId},
+    launch(State).
+
+handle_call(_Request, _From, State) ->
+    {reply, ok, State}.
+
+handle_cast(subscribe, State = #?STATE{datapath_id = DatapathId,
+                                       stats = Stats}) ->
+    [subscribe_reply(DatapathId, Stat) || Stat <- Stats],
+    {noreply, State};
+handle_cast({stop, Reason}, State) ->
+    {stop, Reason, State};
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+handle_info(poll_stats, State = #?STATE{of_version = Version,
+                                        datapath_id = DatapathId,
+                                        stats = Stats}) ->
+    ok = poll_stats(Version, Stats, DatapathId),
+    {noreply, State};
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%% ------------------------------------------------------------------
+%% Internal Function Definitions
+%% ------------------------------------------------------------------
+
+% only subscribe and start time if we enabled.
+launch(State = #?STATE{stats_interval = disable}) ->
+    {ok, State};
+launch(State = #?STATE{stats_interval = StatsInterval}) ->
+    % subscribe before the first poll.
+    gen_server:cast(self(), subscribe),
+    gen_server:cast(self(), poll_stats),
+    IntervalTimer = timer:send_interval(StatsInterval * 1000, poll_stats),
+    {ok, State#?STATE{interval_timer = IntervalTimer}}.
+
+poll_stats(Version, Stats, DatapathId) ->
+    Requests = stats_msgs(Version, Stats),
+    ok = ofs_handler:send_list(DatapathId, Requests).
+
+subscribe_reply(DatapathId, Stat) ->
+    ok = ofs_handler:subscribe(DatapathId, simple_ne_folsom, stats_reply(Stat)).
+
+stats_msgs(Version, Stats) ->
+    [stats_msg(Version, Stat) || Stat <- Stats].
+
+stats_msg(Version, flow) ->
+    of_msg_lib:get_flow_statistics(Version, all, [], []);
+stats_msg(Version, table) ->
+    of_msg_lib:get_table_stats(Version);
+stats_msg(Version, aggregate) ->
+    of_msg_lib:get_aggregate_statistics(Version, all, [], []);
+stats_msg(Version, port) ->
+    of_msg_lib:get_port_statistics(Version, any);
+stats_msg(Version, queue) ->
+    of_msg_lib:get_queue_statistics(Version, any, all);
+stats_msg(Version, group) ->
+    of_msg_lib:get_group_statistics(Version, all);
+stats_msg(Version, meter) ->
+    of_msg_lib:get_meter_stats(Version, all).
+
+stats_reply(flow) ->
+    flow_stats_reply;
+stats_reply(table) ->
+    table_stats_reply;
+stats_reply(aggregate) ->
+    aggregate_stats_reply;
+stats_reply(port) ->
+    port_stats_reply;
+stats_reply(queue) ->
+    queue_stats_reply;
+stats_reply(group) ->
+    group_stats_reply;
+stats_reply(meter) ->
+    meter_stats_reply.
---
+++ stats_poller/apps/simple_ne/src/sne.erl
@@ -0,0 +1,92 @@
+%%------------------------------------------------------------------------------
+%% Copyright 2014 FlowForwarding.org
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%-----------------------------------------------------------------------------
+
+%% @author Erlang Solutions Ltd. <openflow@erlang-solutions.com>
+%% @copyright 2014 FlowForwarding.org
+
+%%% @doc
+%%% Simple network executive command line utility functions.
+%%% @end
+
+-module(sne).
+
+-include_lib("ofs_handler/include/ofs_handler.hrl").
+-include_lib("of_protocol/include/of_protocol.hrl").
+
+-export([
+    tr/0,
+    send/2,
+    sync_send/2,
+    subscribe/2,
+    ping/1,
+    i/1
+]).
+
+%% @hidden
+-spec tr() -> ok.
+tr() ->
+    dbg:start(),
+    dbg:tracer(),
+    dbg:p(all, c),
+    dbg:tpl(ofs_handler_logic, []),
+    dbg:tpl(simple_ne_logic, []),
+    dbg:tpl(simple_ne_ofsh, []),
+    ok.
+
+%% @doc
+%% Send a message to the switch at ``IpAddr''.  Responses are returned
+%% via the ``handle_message'' callback.  To see the messages, you must
+%% create a subscription to the expected replies.
+%% @end
+-spec send(ipaddress(), ofp_message()) -> not_found | ok | {error, error_reason()}.
+send(IpAddr, Msg) ->
+    simple_ne_logic:send(IpAddr, Msg).
+
+%% @doc
+%% Send a message to the switch at ``IpAddr'' and wait for the reply.
+%% @end
+-spec sync_send(ipaddress(), ofp_message()) -> not_found | {ok, ofp_message()} | {error, error_reason()}.
+sync_send(IpAddr, Msg) ->
+    simple_ne_logic:sync_send(IpAddr, Msg).
+
+%% @doc
+%% Add a subscription.
+%% @end
+-spec subscribe(ipaddress(), subscription_item()) -> ok.
+subscribe(IpAddr, MsgType) ->
+    simple_ne_logic:subscribe(IpAddr, MsgType).
+
+%% @doc
+%% send an echo request to the server.  To see the response, first create
+%% a subscription to the echo response: ``sne:subscribe(IpAddr, echo_reply)''.
+%% @end
+-spec ping(ipaddress()) -> not_found | ok | {error, error_reason}.
+ping(IpAddr) ->
+    Echo = of_msg_lib:echo_request(4, <<>>),
+    send(IpAddr, Echo).
+
+%% @doc
+%% Get information about simple_ne state.
+%%
+%% ``switches'' - list the ip address, protocol version, and datapath id
+%% of the connected switches.
+%% @end
+-spec i(Item:: atom()) -> term().
+i(switches) ->
+    lists:foreach(
+        fun({IpAddr, DatapathId, Version, _, _}) ->
+            io:format("~p: v~p ~p~n", [IpAddr, Version, DatapathId])
+        end, simple_ne_logic:switches()).
