df6936b6e2d9882a906515ab1df84a786af3b4c3
marcsugiyama <sugiyama@acm.org>
2014-05-14 13:12:29 -0400

    Only allow one type of collector at a time.

--- tapestry/apps/tapestry/src/tapestry_sup.erl
+++ tapestry/apps/tapestry/src/tapestry_sup.erl
@@ -29,23 +29,18 @@ init([]) ->
     TapDS = {tap_ds,
                         {tap_ds, start_link, []},
                         permanent, 5000, worker, [tap_ds]},
-    TapAggr = {tap_aggr,
-                        {tap_aggr, start_link, []},
-                        permanent, 5000, worker, [tap_aggr]},
-    TapBatch = {tap_batch,
-                        {tap_batch, start_link, []},
-                        permanent, 5000, worker, [tap_batch]},
     UiTestConfig = tap_config:getconfig(ui_test),
-    Children = defined([
+    FtpdConfig = tap_config:getconfig(ftpd),
+    OpenFlowConfig = tap_config:getconfig(openflow),
+    
+    Children = defined(lists:flatten([
         TapYawsSup,
-        tap_ftpd(UiTestConfig),
+        tap_ftpd(UiTestConfig, FtpdConfig, OpenFlowConfig),
         TapClientData,
         TapDS,
-        TapAggr,
-        TapBatch,
-        tap_loom(UiTestConfig),
-        test_ui(UiTestConfig)
-    ]),
+        tap_loom(UiTestConfig, FtpdConfig, OpenFlowConfig),
+        test_ui(UiTestConfig, FtpdConfig, OpenFlowConfig)
+    ])),
     {ok, {{one_for_one, 5, 10}, Children}}.
 
 defined(L) ->
@@ -54,20 +49,30 @@ defined(L) ->
            (_)         -> true
         end, L).
 
-test_ui(disabled) ->
+test_ui(disabled, _, _) ->
     undefined;
-test_ui(enabled) ->
+test_ui(enabled, _, _) ->
     {tap_test_ui, {tap_test_ui, start_link, []},
         permanent, 5000, worker, [tap_test_ui]}.
 
-tap_ftpd(disabled) ->
+tap_ftpd(disabled, enabled, disabled) ->
+    [{tap_batch,
+        {tap_batch, start_link, []},
+        permanent, 5000, worker, [tap_batch]},
     {tap_ftpd, {tap_ftpd, start_link, []},
-        permanent, 5000, worker, [tap_ftpd]};
-tap_ftpd(enabled) ->
+        permanent, 5000, worker, [tap_ftpd]}];
+tap_ftpd(enabled, _, _) ->
+    undefined;
+tap_ftpd(_, disabled, _) ->
     undefined.
 
-tap_loom(disabled) ->
-    {tap_loom, {tap_loom, start_link, []},
-        permanent, 5000, worker, [tap_loom]};
-tap_loom(enabled) ->
+tap_loom(disabled, disabled, enabled) ->
+    [{tap_loom, {tap_loom, start_link, []},
+        permanent, 5000, worker, [tap_loom]},
+    {tap_aggr,
+        {tap_aggr, start_link, []},
+        permanent, 5000, worker, [tap_aggr]}];
+tap_loom(enabled, _, _) ->
+    undefined;
+tap_loom(_, _, disabled) ->
     undefined.
