6ac53640d98f2ab80e3bb23c994535983e78d261
Ira Dostanko <dostanko@gmail.com>
2014-06-17 21:40:34 +0300

    Initial Commit

---
+++ tapestry/ui/mobile/mixed/Tapestry/Tapestry/NCIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NCIAppDelegate.h
+//  Tapestry
+//
+//  Created by Ira on 6/17/14.
+//  Copyright (c) 2014 Ira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NCIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
