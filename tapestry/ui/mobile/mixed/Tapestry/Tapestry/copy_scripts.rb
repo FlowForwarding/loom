6a06c8f2d6adffeff44cd54e6ea81acca7a9dcaa
Ira Dostanko <dostanko@gmail.com>
2014-08-12 18:36:56 +0300

    Pick images as well for Hybrid app from Web resources

--- tapestry/ui/mobile/mixed/Tapestry/Tapestry/copy_scripts.rb
+++ tapestry/ui/mobile/mixed/Tapestry/Tapestry/copy_scripts.rb
@@ -1,15 +1,17 @@
 require 'json'
 require 'fileutils'
 
-fromPath = File.expand_path("../../../../apps/tapestry/priv/www/") + "/"
+fromPath = File.expand_path("../../../../apps/tapestry/priv/www") + "/"
 toPath = "Tapestry/www/src/"
 FileUtils.mkdir_p("Tapestry/www/src")
 
 scripts =  JSON.parse( IO.read(fromPath + "/app_scripts")) +
-JSON.parse( IO.read(fromPath + "/app_styles")) +
-["nci.html", "app_scripts", "app_styles", "static/server_emulator.js", "output.json"]
+  JSON.parse( IO.read(fromPath + "/app_styles")) +
+  Dir[fromPath + "static/images/*"].collect!{|item| "static/images/" + File.basename(item)} +
+  ["nci.html", "app_scripts", "app_styles", "static/server_emulator.js", "output.json"]
 
 scripts.each do |script|
   FileUtils.mkdir_p(File.dirname(toPath + script))
   FileUtils.cp(fromPath + script, toPath + script)
 end
+
