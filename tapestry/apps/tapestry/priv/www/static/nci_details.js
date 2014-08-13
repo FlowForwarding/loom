3170de6aa898728e84a08143e987eb182561ba72
Ira Dostanko <dostanko@gmail.com>
2014-08-13 21:54:54 +0300

    Basics for new tab morkup on NCIDetails page
    unify and simplify Social graph drawance

--- tapestry/apps/tapestry/priv/www/static/nci_details.js
+++ tapestry/apps/tapestry/priv/www/static/nci_details.js
@@ -3,6 +3,7 @@ NCI.detailsTime = $("#detailsTime");
 NCI.detailsFlows = $("#detailsFlows");
 NCI.detailsEndpoints = $("#detailsEndpoints");
 NCI.maxActivitySize = 0;
+NCI.Social = {}
 
 NCI.setupCommunities = function(data){
 	NCI.Communities = data.Communities;
@@ -16,71 +17,74 @@ NCI.setupCommunities = function(data){
 	NCI.detailsNCI.html(NCI.timestampNCI);
 	NCI.detailsTime.html(NCI.parceDateForLastUpdate(NCI.timestamp));
 	
-	NCI.socialGraph.endpoints = 0;
+	NCI.Social.endpoints = 0;
 	var sizesSum = 0;
 	$.each(NCI.Communities, function(index, community){
-		NCI.socialGraph.endpoints += community.Endpoints.length;
+		NCI.Social.endpoints += community.Endpoints.length;
 		sizesSum  += community.Size;
 	});
 	
 	NCI.detailsEndpoints.html(NCI.parceNumberForView(sizesSum));
 };
 
-NCI.socialGraph  = (function(){
-	var me = $("#socialGraph");
+NCI.socialGraph = function(socialGraphID){
+	var me = $(socialGraphID);
 	
 	var force;
+	var socialGraphSelector = socialGraphID + " .socialGraph"
 	var color = d3.scale.category10();
 	var notNetworkColor = "#000000";
 	var isClustered = false;
-	var isActivities = false;
 	var isDevided = false;
 	var isFiltered = false;
+	var selectedDots = {}
 	me.graphBuilder;
+	me.numOfPoints = 0;
+	me.isExpandable = false;
 	
-	me.show = function(devided, clustered, filtered, activities){
+	me.find('.byactivities').on('click', function(event){
+		isDevided = this.checked;
+		me.show();
+	});
+	me.find('.pretty').on('click', function(event){
+		isClustered = this.checked;
+		me.show();
+	});
+	me.find('.internal').on('click', function(event){
+		isFiltered = this.checked;
+		me.show();
+	});
+	
+	me.show = function(needDraw){
 		if (NCI.Communities.length == 0){
 			return;
-		};	
-		//if we change type of graph (flows/activities) or need to redraw it from sratch
-		if (isActivities != activities || !me.graphBuilder || (isClustered && !clustered)){
+		};
+		if (needDraw) {
 		    d3.select("#activities_graph").remove();
-			var numOfPoints =  activities ?  NCI.CommunityGraph.Endpoints.length : NCI.socialGraph.endpoints;
-			if (numOfPoints > NCI.max_vertices) {
-				d3.select("#activities_graph").remove();
-				d3.select("#socialGraph").append('text')
+			if (me.numOfPoints > NCI.max_vertices) {
+				d3.select(socialGraphSelector).append('text')
 				.attr("id","activities_graph")
 				.html('Too many endpoints to draw');
 			} else {
-				me.graphBuilder = activities ? new NCI.graphBuilder([NCI.CommunityGraph]) : new NCI.graphBuilder(NCI.Communities);
-				isClustered = clustered;
-				isActivities = activities;
-				isDevided = devided;
-				isFiltered = filtered;
-			    me.draw(devided, clustered, filtered);
-				isClustered = clustered;
+			    me.draw();
 			}
 		//otherwise just change appearance	
 		} else {
-			isClustered = clustered;
-			isActivities = activities;
-			isDevided = devided;
-			isFiltered = filtered;
-		    me.setupNodes(filtered, devided, clustered);
+		    me.setupNodes();
 		    force.start();
 		}
 	};
 	
 	//colorify and set radius
-	me.setupNodes = function(filtered, devided, clustered){
+	me.setupNodes = function(){
 		me.node.style("fill", function(d) {
 			//if this dot is selected on Activities graph, draw it in red
 			if (NCI.Social.selectedDot == d.name.split(":")[0])
 			    return "FF0000";
-		    if ( filtered && NCI.isExternal(d.name)){
+		    if ( isFiltered && NCI.isExternal(d.name)){
 			    return notNetworkColor;
 		    };
-		    return devided ? color(d.group) : color(0);
+		    return isDevided ? color(d.group) : color(0);
 		});
 		me.node.attr("r", function(d) { 
 			var radius = 4;
@@ -89,19 +93,19 @@ NCI.socialGraph  = (function(){
 		 	}
 			return radius;
 		});
-		force.linkStrength(clustered ? 1 : 0);
+		force.linkStrength(isClustered ? 1 : 0);
 	};
 	
-	me.draw = function(devided, clustered, filtered){	
+	me.draw = function(){	
 		var graphWidth = me.width();
 		var graphHeight = $('#nciDetails').height() - 200;
 		force = d3.layout.force()
 		    .charge(-20)
 			.linkDistance(30)
 			.size([graphWidth,  graphHeight])
-			.linkStrength(clustered ? 1 : 0);
+			.linkStrength(isClustered ? 1 : 0);
 			
-	    me.activitiesGraphSvg = d3.select("#socialGraph").append("svg")
+	    me.activitiesGraphSvg = d3.select(socialGraphSelector).append("svg")
 		    .attr("id","activities_graph")
 			.attr("width", graphWidth)
 			.attr("height", graphHeight);
@@ -115,7 +119,6 @@ NCI.socialGraph  = (function(){
 			linksData.exit().remove();
 		};
 		
-		NCI.Social.selectedDots = {}
 		var setupNodes = function(){
 			force.nodes(me.graphBuilder.graph.nodes);
 			me.activitiesGraphSvg.selectAll("circle").remove();
@@ -124,29 +127,29 @@ NCI.socialGraph  = (function(){
             nodesData.exit().remove();
 			me.setupNodes(isFiltered, isDevided, isClustered);
 			me.node.append("title").html(function(d) { return d.fullname + "<br>" + d.connections + " connections" ; });
-			if (isActivities) {
+			if (me.isExpandable) {
 				me.node.on('click', function(d){
 					var label = d.name.split(":")[0];
-					var group = NCI.Social.selectedDots[label]
+					var group = selectedDots[label]
 					if (group !== undefined){
-						delete NCI.Social.selectedDots[NCI.Social.selectedDots[label]]
-						delete NCI.Social.selectedDots[label]
+						delete selectedDots[selectedDots[label]]
+						delete selectedDots[label]
 						me.graphBuilder.removeCommunity(group);
 						setupLinks();		
 						setupNodes();
 						force.start();
 						return;
 					}
-					for (var i=0; i < Object.keys(NCI.Social.selectedDots).length + 1; i++){
-						if (NCI.Social.selectedDots[i + 1] === undefined){
-							NCI.Social.selectedDots[i + 1] = true;
-							NCI.Social.selectedDots[label] =  i + 1;
+					for (var i=0; i < Object.keys(selectedDots).length + 1; i++){
+						if (selectedDots[i + 1] === undefined){
+							selectedDots[i + 1] = true;
+							selectedDots[label] =  i + 1;
 							break
 						}
 					}
 					$.each(NCI.Communities, function(index, community){
 						if (community.Label == label){
-							me.graphBuilder.addCommunity(community, label, NCI.Social.selectedDots[label] );
+							me.graphBuilder.addCommunity(community, label, selectedDots[label] );
 							setupLinks();		
 							setupNodes();
 							force.start();
@@ -172,9 +175,7 @@ NCI.socialGraph  = (function(){
 	};
 	
 	return me;
-}());
-
-NCI.Social = {}
+};
 
 NCI.graphBuilder = function(communities){
 	var thisBuilder = this;
@@ -257,14 +258,12 @@ NCI.graphBuilder = function(communities){
 $(".hide-ncidetails").on('click', function(){
 	$('#nciDetails').removeClass('details-view-show');
 	NCI.nciHistogram.clean();
-	NCI.socialGraph.text("");
 	NCI.CommunityGraph = [];
 	NCI.Communities = [];
 	NCI.detailsEndpoints.html("");
 	NCI.detailsNCI.html("");
 	NCI.detailsTime.html("");
 	NCI.detailsFlows.html("");
-	NCI.socialGraph.graphBuilder = undefined;
 	$($('#nciDetailsTabs').find("dd a")[0]).click();
 });
 
@@ -272,29 +271,19 @@ $(".hide-ncidetails").on('click', function(){
 $('#nciDetailsTabs').on('toggled', function (event, tab) {
 	switch(tab[0].id) {
 	    case "panelFlows":
-	        NCI.socialGraph.show( false, false, false, false);
-	        break;
-	    case "panelFlowsByActivities":
-	        NCI.socialGraph.show(true, false, false, false);
-	        break;
-	    case "panelFlowsPretty":
-	        NCI.socialGraph.show(true, true, false, false);
-	        break;
-	    case "panelInternalFlows":
-	        NCI.socialGraph.show(true, true, true, false);
+			var flowsPanel = new NCI.socialGraph("#panelFlows");
+			flowsPanel.graphBuilder  = new NCI.graphBuilder(NCI.Communities);
+			flowsPanel.numOfPoints =  NCI.Social.endpoints;
+	        flowsPanel.show(true);
 	        break;
 	    case "panelActivities":
-	        NCI.socialGraph.show(false, false, false, true);
+			var activitiesPanel = new NCI.socialGraph("#panelActivities");
+			activitiesPanel.graphBuilder  = new NCI.graphBuilder([NCI.CommunityGraph]);
+			activitiesPanel.numOfPoints = NCI.CommunityGraph.Endpoints.length;
+			activitiesPanel.isExpandable = true;
+			activitiesPanel.show(true);
 	        break;
-	    case "panelActivitiesPretty":
-	        NCI.socialGraph.show(true, true, false, true);
-	        break;
-	    case "panelActivitiesInternal":
-	        NCI.socialGraph.show(true, true, true, true);
-	        break;				
 	    default:
 			NCI.nciHistogram.show();
-			NCI.socialGraph.text("");
-			NCI.socialGraph.graphBuilder = undefined;
 	};
 });
