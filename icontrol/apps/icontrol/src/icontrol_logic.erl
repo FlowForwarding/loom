656db83ceb5b5fbedc0c7f1196fbdc186b683981
Ruan Pienaar <ruan800@gmail.com>
2014-09-10 13:35:37 +0100

    removed old digraph work, and added priv linc-oe repo's as deps

--- icontrol/apps/icontrol/src/icontrol_logic.erl
+++ icontrol/apps/icontrol/src/icontrol_logic.erl
@@ -36,8 +36,7 @@
 -record(?STATE, {
     next_switch_key = 1 :: integer(),
     switches_table :: term(), % ets table
-    default_switch :: switch_key(),
-    dg_state :: term()
+    default_switch :: switch_key()
 }).
 
 -record(switch_record, {
@@ -79,9 +78,7 @@
     close_connection/1,
     set_default/1,
     show_default/0,
-    ofs_version/1,
-    re_init_dg/0
-]).
+    ofs_version/1]).
 
 start_link() ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
@@ -186,19 +183,14 @@ show_default() ->
 ofs_version(SwitchKey) ->
     gen_server:call(?SERVER, {ofs_version, SwitchKey}).
 
-re_init_dg() ->
-    gen_server:call(?SERVER, re_init_dg).
-
 %% ------------------------------------------------------------------
 %% gen_server Function Definitions
 %% ------------------------------------------------------------------
 
 init([]) ->
     SwitchesTable = ets:new(switches, [{keypos, 2}, set, protected]),
-    DGState = icontrol_dg:init(),
     State = #?STATE{switches_table = SwitchesTable,
-                    default_switch = undefined,
-                    dg_state = DGState
+                    default_switch = undefined
                    },
     {ok, State}.
 
@@ -235,9 +227,6 @@ handle_call(switches, _From, State) ->
             connection = Connection
         } <- do_get_switches(State)],
     {reply, Reply, State};
-handle_call(re_init_dg,_From,State) ->
-    DGState = icontrol_dg:init(),
-    {reply,DGState,State#?STATE{ dg_state = DGState }};
 handle_call(_Request, _From, State) ->
     {reply, ok, State}.
 
@@ -259,8 +248,7 @@ code_change(_OldVsn, State, _Extra) ->
 
 register_switch(IpAddr, DatapathId, Version, Connection,
                             State = #?STATE{switches_table = Switches,
-                                            next_switch_key = SwitchKey,
-                                            dg_state = DGState }) ->
+                                            next_switch_key = SwitchKey }) ->
     SwitchRecord = #switch_record{
         switch_key = SwitchKey,
         ipaddr = IpAddr,
@@ -270,13 +258,7 @@ register_switch(IpAddr, DatapathId, Version, Connection,
     },
     true = ets:insert(Switches, SwitchRecord),
     NewState = maybe_set_switch_key(SwitchKey, State),
-
-    %% N: Only adding switces to DG not removing...
-    Labels = [{ip,IpAddr},{version,Version}],
-    NewDGState = icontrol_dg:add_switch(DGState,DatapathId,Labels,SwitchKey),
-
-    NewState#?STATE{next_switch_key = SwitchKey + 1,
-                    dg_state = NewDGState}.
+    NewState#?STATE{next_switch_key = SwitchKey + 1}.
 
 maybe_set_switch_key(Key, State = #?STATE{default_switch = undefined}) ->
     State#?STATE{default_switch = Key};
