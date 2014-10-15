cac901b5b26bcab7275f90dfc234186813d45c58
Ruan Pienaar <ruan800@gmail.com>
2014-10-15 11:58:52 +0100

    net_observer work

--- net_observer/apps/net_observer/src/net_observer_sup.erl
+++ net_observer/apps/net_observer/src/net_observer_sup.erl
@@ -23,5 +23,7 @@ start_link() ->
 %% ===================================================================
 
 init([]) ->
-    {ok, { {one_for_one, 5, 10}, []} }.
+    {ok, { {one_for_one, 5, 10}, [
+        ?CHILD(net_observer_logic, worker)
+    ]} }.
 
