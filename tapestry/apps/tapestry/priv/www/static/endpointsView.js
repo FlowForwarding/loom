f5b3e51700ba086023686d27d8961bc6239e77c7
Aliaksandr Kazarez <kozorezal@gmail.com>
2014-11-06 17:16:39 +0300

    Implemented Histogram view for endpoints
    Added histogram view with possibility to sort data and paging

--- tapestry/apps/tapestry/priv/www/static/endpointsView.js
+++ tapestry/apps/tapestry/priv/www/static/endpointsView.js
@@ -23,20 +23,76 @@
 
 
     function EndpointsView($container) {
-        console.time("parsing activities");
         NCI.model.parseActivities(NCI.Communities);
-        console.timeEnd("parsing activities");
 
         var container = $container.get(0),
-            endpoints = NCI.model.endpoints(),
+            endpoints = sortEndpoints(NCI.model.endpoints(), "totalConnections", "desc"),
             d3Container = d3.select(container),
             activitiesListContainer = d3Container.select(".activities-list"),
+            endpointsHistogramContainer = d3Container.select(".endpoints-histogram"),
             table = new NCI.Table(activitiesListContainer, columns, endpoints),
+            histogram = new NCI.EndpointsHistogram(endpointsHistogramContainer, endpoints),
             topHundred = {
                 name: "All Endpoints",
-                connections: endpoints
+                endpoints: endpoints
             },
-            breadcrumbsData = [topHundred];
+            breadcrumbsData = [topHundred],
+            $histogramView = $container.find(".show-histogram"),
+            $tableView = $container.find(".show-table"),
+            $endpointSortMenu = $container.find(".endpoint-sort");
+
+
+        $histogramView.on("click", function() {
+            $histogramView.hide();
+            $tableView.show();
+            $(activitiesListContainer.node()).hide();
+            $(endpointsHistogramContainer.node()).show();
+        });
+
+        $tableView.on("click", function() {
+            $tableView.hide();
+            $histogramView.show();
+            $(activitiesListContainer.node()).show();
+            $(endpointsHistogramContainer.node()).hide();
+        });
+
+        function sortEndpoints(endpoints, field, direction) {
+            direction = direction === "desc" ? -1 : 1;
+            return endpoints.sort(function(item1, item2) {
+                var res = (item1[field] - item2[field])*direction;
+                if (res===0) {
+                    res = item1.ip > item2.ip ? 1 : -1;
+                }
+                return res;
+            });
+        }
+
+        function triggerActive($el, shouldSet) {
+            $el.parents("[data-sort-field]").toggleClass("active", shouldSet);
+            $el.toggleClass("active", shouldSet);
+        }
+
+        var $prevSort = $endpointSortMenu.find("[data-sort-field=totalConnections] [data-sort-direction=desc]");
+        triggerActive($prevSort, true);
+
+        $endpointSortMenu.on("click", "[data-sort-direction]", function() {
+            var $el = $(this),
+                sortDirection = $el.data("sortDirection"),
+                sortField = $el.parents("[data-sort-field]").data("sortField"),
+                endpoint = breadcrumbsData[breadcrumbsData.length - 1].endpoint,
+                currentData = endpoint ?
+                    Object.keys(endpoint.connections).map(function(key) {return endpoint.connections[key]}) :
+                    endpoints;
+
+            triggerActive($prevSort, false);
+            triggerActive($el, true);
+
+            $prevSort = $el;
+
+            currentData = sortEndpoints(currentData, sortField, sortDirection);
+            console.log("sorted?");
+            setEndpoints(currentData);
+        });
 
         function updateBreadcrumbs() {
             var length = breadcrumbsData.length,
@@ -59,17 +115,21 @@
                     return (length - 1) == index;
                 })
                 .on("click", function(d, index) {
-                    if (d!== topHundred) {
+                    if (d !== topHundred) {
                         setActiveEndpoint(d.endpoint);
                     } else {
                         breadcrumbsData = [topHundred];
                         updateBreadcrumbs();
-                        table.setData(endpoints);
+                        setEndpoints(topHundred.endpoints);
                     }
-
                 });
 
             return breadcrumbs;
+        }
+
+        function setEndpoints(endpoints) {
+            table.setData(endpoints);
+            histogram.setData(endpoints);
         }
 
         function setActiveEndpoint(endpoint) {
@@ -93,16 +153,21 @@
             }
 
             updateBreadcrumbs();
-
-            table.setData(endpoints);
+            setEndpoints(endpoints);
 
         }
 
         updateBreadcrumbs(breadcrumbsData);
 
-        $(table).on("click", function(event, data) {
+        $([table, histogram]).on("click", function(event, data) {
             setActiveEndpoint(data);
         });
+
+        $histogramView.click();
+
+        // trigger resize here in order to adjust histogram height;
+        // TODO: get rid of this hack
+        $(window).resize();
     }
 
     NCI.EndpointsView = EndpointsView;
