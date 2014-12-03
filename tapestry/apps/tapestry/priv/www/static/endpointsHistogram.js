fd5cf40f59c2fed63b84e221eb1edb6181fdbe72
Aliaksandr Kazarez <kozorezal@gmail.com>
2014-12-03 17:37:59 +0300

    added legend to endpoints histogram

--- tapestry/apps/tapestry/priv/www/static/endpointsHistogram.js
+++ tapestry/apps/tapestry/priv/www/static/endpointsHistogram.js
@@ -2,7 +2,7 @@
 
     var color = d3.scale.category20(),
         showHostname = false,
-        margin = {top: 50, right: 70, bottom: 40, left: 100};
+        margin = {top: 50, right: 70, bottom: 100, left: 100};
 
     $(NCI).on("showHostnames", function(e, show) {
         showHostname = show;
@@ -159,7 +159,7 @@
         ;
 
         var yAxisText = "Number of connected endpoints",
-            xAxisText = "Endpoints";
+            xAxisText = "Endpoints sorted by number of connected endpoints";
 
         chart.selectAll(".axis-caption")
             .remove();
@@ -168,7 +168,7 @@
             .classed("axis-caption", true)
             .attr('style', 'font-weight:bold')
             .html(xAxisText)
-            .attr('transform', "translate(" + (width/2 - margin.left + 30) +", " + (height + 20) + ")"); // 30 is height of xaxis
+            .attr('transform', "translate(" + (width/2 - margin.left - 50) +", " + (height + 20) + ")"); // 30 is height of xaxis
 
         chart.append('text')
             .classed("axis-caption", true)
@@ -176,8 +176,64 @@
             .html(yAxisText)
             .attr('transform', "rotate(-90) translate(" + (-height + margin.top) + ", " + (-30) + ") ");
 
+        chart.call(updateLegends, height);
     }
 
+
+    function updateLegends(chart, height) {
+
+        function createContainer(legends, text, cls) {
+            var container = legends.append("g")
+                .attr("class", cls)
+
+            container.append("text")
+                .text(text);
+
+            return container;
+        }
+
+        function createLegendItem(container, cls, caption) {
+            var legendContainer = container.append("g");
+
+            legendContainer.append("rect")
+                .attr({
+                    x: 0,
+                    y: 0,
+                    width: "20",
+                    height: "20",
+                    class: cls,
+                    transform: "translate(0, -15)"
+                });
+
+            legendContainer.append("text")
+                .attr("transform", "translate(25, 0)")
+                .text("- " + caption);
+
+            return legendContainer;
+        }
+
+        chart.selectAll(".endpoints-legend")
+            .remove();
+
+        var legendsContainer = chart.append("g")
+            .attr("class", "endpoints-legend")
+            .attr("transform", "translate(0," + (height + 40) + ")");
+
+        var internalContainer = createContainer(legendsContainer, "Internal endpoints:");
+
+        var externalContainer = createContainer(legendsContainer, "External endpoints:", "external")
+            .attr("transform", "translate(300, 0)");
+
+        createLegendItem(externalContainer, "internal-connections", "Number of Internal connections")
+            .attr("transform", "translate(0, 25)");
+        createLegendItem(externalContainer, "external-connections", "Number of External connections")
+            .attr("transform", "translate(0, 50)");
+
+        createLegendItem(internalContainer, "internal-connections", "Number of Internal connections")
+            .attr("transform", "translate(0, 25)");
+        createLegendItem(internalContainer, "external-connections", "Number of External connections")
+            .attr("transform", "translate(0, 50)");
+    }
 
     function EndpointsHistogram(container, data, options) {
         options = options || {};
