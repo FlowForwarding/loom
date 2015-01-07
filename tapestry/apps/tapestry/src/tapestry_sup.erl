6e29ba805f618cbc2a5a7bb16a2e9ab91d5dec72
marcsugiyama <sugiyama@acm.org>
2015-01-06 19:09:12 -0800

    Read tapestry.config values before they are used.

--- tapestry/apps/tapestry/src/tapestry_sup.erl
+++ tapestry/apps/tapestry/src/tapestry_sup.erl
@@ -20,6 +20,8 @@ start_link() ->
 %% ===================================================================
 
 init([]) ->
+    tap_config:refresh(),
+
     TapYawsSup = {tap_yaws_sup,
                         {tap_yaws_sup, start_link, []},
                         permanent, infinity, supervisor, [tap_yaws_sup]},
@@ -51,7 +53,6 @@ init([]) ->
         test_ui(Run(test_ui))
     ]),
     ok = tap_dns:new_cache(),
-    tap_config:refresh(),
     {ok, {{one_for_one, 5, 10}, Children}}.
 
 test_ui(true) ->
